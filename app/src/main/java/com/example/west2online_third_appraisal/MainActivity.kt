b67090b0e42f5ace5873e8919e1b67ae7f88c430
ChenJialong <2833275565@qq.com>
2021-12-18 22:58:11 +0800

    西二第三轮考核(qbyyygq)

---
+++ app/src/main/java/com/example/west2online_third_appraisal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.west2online_third_appraisal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
